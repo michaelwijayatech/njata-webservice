c7bf3a79a22c85ceb71ea84836e532db06c7176d
michaelproperti2019 <michael.properti2019@gmail.com>
2019-05-11 18:49:58 +0700

    20190511

--- app/Http/Controllers/API/DesktopController.php
+++ app/Http/Controllers/API/DesktopController.php
@@ -2651,9 +2651,9 @@ class DesktopController extends Controller
                 $fpdf->Cell(0, 0, 'Gaji Harian');
                 $fpdf->Ln(2);
                 $fpdf->SetFont('Arial', '', 10);
-                $days = strtotime($end_date) - strtotime($start_date);
-                $days = round($days / (60 * 60 * 24));
-                $fpdf->Cell(0, 10, 'Periode : ' . $start_date . ' s/d ' . $end_date . ' || ' . $days);
+                $__days = strtotime($end_date) - strtotime($start_date);
+                $__days = round($__days / (60 * 60 * 24)) + 1;
+                $fpdf->Cell(0, 10, 'Periode : ' . $start_date . ' s/d ' . $end_date . ' || ' . $__days);
                 $fpdf->Ln(10);
 
                 $days = explode("#", $_days);
@@ -2684,8 +2684,8 @@ class DesktopController extends Controller
                 $fpdf->Ln();
 
                 $fpdf->Cell(8+40+8+8,5,' ',0,0,'C');
-                $days = explode("#", $_days);
-                if (count($days) - 1 === 4){
+//                $days = explode("#", $_days);
+                if ($__days === 4){
                     if (explode("-",$days[0])[0] > 24){
                         $fpdf->Cell(8,-8, explode("-",$start_date)[0],1, 0, 'C');
                         $fpdf->Cell(8,-8, explode("-",$start_date)[0]+1,1, 0, 'C');
