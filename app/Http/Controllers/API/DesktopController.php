2d7069a8d75800a0385e2fae7ce0b14fee71060b
michaelproperti2019 <michael.properti2019@gmail.com>
2019-05-11 18:12:52 +0700

    20190511

--- app/Http/Controllers/API/DesktopController.php
+++ app/Http/Controllers/API/DesktopController.php
@@ -2666,7 +2666,7 @@ class DesktopController extends Controller
 //                if ($tot_days >= 5){
 //                    $fpdf->Cell($tot_days*8,8,'Tgl. Pendapatan hari masuk kerja',1,0,'C');
 //                } else {
-                    $fpdf->Cell(5*8,8,'Tgl. Pendapatan hari masuk kerja',1,0,'C');
+                    $fpdf->Cell(6*8,8,'Tgl. Pendapatan hari masuk kerja',1,0,'C');
 //                }
                 $fpdf->SetFont('Arial', 'B', 10);
                 $fpdf->Cell(20,16,'Jumlah',1,0,'C');
