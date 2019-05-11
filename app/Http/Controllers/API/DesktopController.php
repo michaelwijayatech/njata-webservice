88b425c367dea315fadcdb0d4fb5a7e6bbef6ed6
michaelproperti2019 <michael.properti2019@gmail.com>
2019-05-11 18:29:01 +0700

    20190511

--- app/Http/Controllers/API/DesktopController.php
+++ app/Http/Controllers/API/DesktopController.php
@@ -2685,19 +2685,19 @@ class DesktopController extends Controller
                 $days = explode("#", $_days);
                 if (count($days)-1 === 3){
                     if (explode("-",$days[0])[0] > 4){
-                        $fpdf->Cell(8,-8, explode("-",$days[0])[0],1, 0, 'C');
-                        $fpdf->Cell(8,-8, explode("-",$days[1])[0],1, 0, 'C');
-                        $fpdf->Cell(8,-8, explode("-",$days[2])[0],1, 0, 'C');
+                        $fpdf->Cell(8,-8, explode("-",$start_date)[0],1, 0, 'C');
+                        $fpdf->Cell(8,-8, explode("-",$start_date)[0],1, 0, 'C');
+                        $fpdf->Cell(8,-8, explode("-",$start_date)[0],1, 0, 'C');
                         $fpdf->Cell(8,-8, ' ',1, 0, 'C');
                         $fpdf->Cell(8,-8, ' ',1, 0, 'C');
                         $fpdf->Cell(8,-8, ' ',1, 0, 'C');
                     } else {
                         $fpdf->Cell(8,-8, ' ',1, 0, 'C');
                         $fpdf->Cell(8,-8, ' ',1, 0, 'C');
-                        $fpdf->Cell(8,-8, ' ',1, 0, 'C');
-                        $fpdf->Cell(8,-8, explode("-",$days[0])[0],1, 0, 'C');
-                        $fpdf->Cell(8,-8, explode("-",$days[1])[0],1, 0, 'C');
-                        $fpdf->Cell(8,-8, explode("-",$days[2])[0],1, 0, 'C');
+                        $fpdf->Cell(8,-8, explode("-",$start_date)[0], 1, 0, 'C');
+                        $fpdf->Cell(8,-8, explode("-",$start_date)[0] +1,1, 0, 'C');
+                        $fpdf->Cell(8,-8, explode("-",$start_date)[0] +2,1, 0, 'C');
+                        $fpdf->Cell(8,-8, explode("-",$start_date)[0] +3,1, 0, 'C');
                     }
                 }
 
